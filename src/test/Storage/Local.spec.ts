9cca0458ad6499cc7b99f784158ba28104ffa771
IInfinity <DevinDon@Foxmail.com>
2019-04-11 23:56:45 +0800

    test(Storage): add Storage test

---
+++ src/test/Storage/Local.spec.ts
@@ -0,0 +1,36 @@
+import { Local, Storage, Option, Session } from '../../main';
+
+describe('local storage test', () => {
+
+  let storage: Storage;
+
+  beforeAll(done => {
+    const option: Option = {
+      name: 'session.id',
+      expire: 0,
+      domain: 'localhost',
+      koa: {} as any,
+      redis: {
+        host: 'a-1.don.red'
+      },
+      secert: ['secert', 'key']
+    };
+    storage = new Local(option);
+    done();
+  });
+
+  it('should be set correctly', async done => {
+    const session: Session = { __expireAt: Date.now() + 10 * 1000, id: '1', text: Date.now() };
+    expect(await storage.set(session)).toBeTruthy();
+    expect(await storage.get<Session>(session.id)).toBe(session);
+    done();
+  });
+
+  it('should expire correctly', async done => {
+    const session: Session = { __expireAt: Date.now(), id: '1', text: Date.now() };
+    expect(await storage.set(session)).toBeTruthy();
+    expect(await storage.get(session.id)).toBeUndefined();
+    done();
+  });
+
+});
