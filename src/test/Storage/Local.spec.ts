87c02e0eb448de9ab297fa323b15e43eb7e2d9de
IInfinity <DevinDon@Foxmail.com>
2019-04-12 15:38:48 +0800

    test(local): fix expire test

--- src/test/Storage/Local.spec.ts
+++ src/test/Storage/Local.spec.ts
@@ -3,11 +3,12 @@ import { Local, Storage, Option, Session } from '../../main';
 describe('local storage test', () => {
 
   let storage: Storage;
+  let session: Session;
 
   beforeAll(done => {
     const option: Option = {
       name: 'session.id',
-      expire: 0,
+      expire: 1,
       domain: 'localhost',
       koa: {} as any,
       redis: {
@@ -19,18 +20,28 @@ describe('local storage test', () => {
     done();
   });
 
+  beforeEach(done => {
+    session = { id: '1', text: Date.now() };
+    done();
+  });
+
+  afterEach(done => {
+    storage.delete(session.id);
+    done();
+  });
+
   it('should be set correctly', async done => {
-    const session: Session = { __expireAt: Date.now() + 10 * 1000, id: '1', text: Date.now() };
     expect(await storage.set(session)).toBeTruthy();
     expect(await storage.get<Session>(session.id)).toBe(session);
     done();
   });
 
   it('should expire correctly', async done => {
-    const session: Session = { __expireAt: Date.now(), id: '1', text: Date.now() };
     expect(await storage.set(session)).toBeTruthy();
-    expect(await storage.get(session.id)).toBeUndefined();
-    done();
+    setTimeout(async () => {
+      expect(await storage.get(session.id)).toBeUndefined();
+      done();
+    }, 1500);
   });
 
 });
