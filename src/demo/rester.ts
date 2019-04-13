41909a04b0dbcba20e564c6e23ceb3e41bb9beaf
IInfinity <DevinDon@Foxmail.com>
2019-04-13 09:34:49 +0800

    fix(demo): fix demo

--- src/demo/rester.ts
+++ src/demo/rester.ts
@@ -7,7 +7,7 @@ const rester = new Rester(/** ... */);
 const option: Option = {
   domain: 'your.domain.com',
   expire: 1000, // second
-  koa: rester.koa, // your koa instance, for signing cookie
+  koa: rester.app, // your koa instance, for signing cookie
   name: 'session.id', // cookie key of session id
   secert: ['your', 'secert', 'keys'] // signed your cookie with this array
 };
